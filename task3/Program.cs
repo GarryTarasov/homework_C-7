300aba7c37ed48a1db99e4099cc82d699593b8d0
Garry <igor_tarasov_84@mail.ru>
2022-08-19 20:13:05 +0500

    end homework 7

--- task3/Program.cs
+++ task3/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+//Например, задан массив:
+//1 4 7 2
+//5 9 2 3
+//8 4 2 4
+//Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+Console.Clear();
+
+
+int InputInt(string output) {     
+     Console.Write(output);     
+    return Convert.ToInt32(Console.ReadLine());
+}
+int m = InputInt("Введите число строк массива: ");
+int n = InputInt("Введите число граф массива: ");
+Console.WriteLine("Массив: ");
+int[,] array = new int[m, n];
+Random random = new Random();    
+for (int i = 0; i < m; i++){
+    for (int j = 0; j < n; j++){
+        array[i, j] = random.Next(0, 10);
+        Console.Write(array[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+
+int sum = 0;
+double sredAr = 0;
+for (int i = 0; i < n; i++){
+    for (int j = 0; j < m; j++){
+        sum+= array[j,i];
+    }
+    sredAr = (double)sum/m;
+    sum = 0;
+    Console.WriteLine("Среднее арифметическое столбца " + (i+1)+ " = " + Math.Round(sredAr, 1));
+}
